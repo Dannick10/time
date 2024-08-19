e4b10e0364b08df4ad06cd83f4b1798c045c3032
unknown <danmaggot2@gmail.com>
2024-08-19 20:14:55 -0300

    test

--- src/services/useGetLocaleAPI.ts
+++ src/services/useGetLocaleAPI.ts
@@ -7,18 +7,19 @@ import { Weather } from "@/types/Weather";
 export default function useGetLocaleAPI() {
 
   const [data, setData] = useState(null);
-  const [city, setCity] = useState<string>("aracaju");
-  const [loading, setLoading] = useState<boolean>(false);
+  const [city, SetCity] = useState<string>("aracaju");
+  const [loading, SetLoading] = useState<boolean>(false);
   const [weather, setWeather] = useState<Weather | null>(null);
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<boolean | null>(null);
   const api_key = process.env.NEXT_PUBLIC_KEY;
+
 
   const fetchData = async () => {
     const countryCode = "BR";
     const language = "pt";
     const limit = 1;
-    const link = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${countryCode}&limit=${limit}&appid=${api_key}&lang=${language}`;
-    setLoading(true);
+    const link = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${countryCode}&limit=${limit}&appid=${api_key}&lang=${language}`;
+    SetLoading(true)
     try {
       const response = await axios.get(link);
       const locationData = response.data[0];
@@ -31,16 +32,16 @@ export default function useGetLocaleAPI() {
   };
 
   const fetchDataWeather = async (lat: string, lon: string) => {
-    const link = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&lang=pt&units=metric`;
+    const link = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&lang=pt&units=metric`;
 
     try {
       const response = await axios.get(link);
       setWeather(response.data);
       console.log(response.data);
     } catch (error: any) {
-      setError(error.message);
+      setError(error);
     }
-    setLoading(false);
+    SetLoading(false)
   };
 
   return {
@@ -48,7 +49,7 @@ export default function useGetLocaleAPI() {
     error,
     fetchData,
     weather,
-    setCity,
+    SetCity,
     loading
   };
 }
